8349338cd6c227199293682c9bc0fccc42bfa3cd
rahul patil <rahul.patil3@synechron.com>
2012-03-28 14:17:54 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProTemplateApp31::Application.config.secret_token = 'c41acc1182ffe24a405eca4affd76490d10c71ea6dabef36b1880cb3c40a6e3c251b084d4026911b8f95becf54f38f5764e5b16cede1553c8b42aefc858b3812'
